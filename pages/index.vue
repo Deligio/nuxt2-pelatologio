57e2c35ffaab176f465041dec9503631b8b1c295
PanagiotisDeligio <93537254+PanagiotisDeligio@users.noreply.github.com>
2023-05-29 00:04:16 +0300

    add test calendar on dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -10,65 +10,29 @@
           Welcome to the Vuetify + Nuxt.js template
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
+          <v-row>
+            <v-col>
+              <v-sheet height="400">
+                <v-calendar
+                  ref="calendar"
+                  :now="today"
+                  :value="today"
+                  :events="events"
+                  color="primary"
+                  type="week"
+                ></v-calendar>
+              </v-sheet>
+            </v-col>
+          </v-row>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
           <v-btn
             color="primary"
             nuxt
-            to="/inspire"
+            to="/services/new"
           >
-            Continue
+            New Service
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -79,5 +43,52 @@
 <script>
 export default {
   name: 'IndexPage',
+  data: () => ({
+    today: '2019-01-08',
+    events: [
+      {
+        name: 'Weekly Meeting',
+        start: '2019-01-07 09:00',
+        end: '2019-01-07 10:00',
+      },
+      {
+        name: `Thomas' Birthday`,
+        start: '2019-01-10',
+      },
+      {
+        name: 'Mash Potatoes',
+        start: '2019-01-09 12:30',
+        end: '2019-01-09 15:30',
+      },
+    ],
+  }),
+  mounted () {
+    this.$refs.calendar.scrollToTime('08:00')
+  },
 }
 </script>
+
+<style scoped>
+.my-event {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  border-radius: 2px;
+  background-color: #1867c0;
+  color: #ffffff;
+  border: 1px solid #1867c0;
+  font-size: 12px;
+  padding: 3px;
+  cursor: pointer;
+  margin-bottom: 1px;
+  left: 4px;
+  margin-right: 8px;
+  position: relative;
+}
+
+.my-event.with-time {
+  position: absolute;
+  right: 4px;
+  margin-right: 0px;
+}
+</style>
